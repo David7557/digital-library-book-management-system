a1d717f827177fb191f0a7989ef432ad5e4bd006
David7557 <davidkumar7557@gmail.com>
2025-03-27 14:30:11 +0530

    project initializing

---
+++ src/main/java/com/ajackus/digital_library_book_management_system/DigitalLibraryBookManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ajackus.digital_library_book_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLibraryBookManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalLibraryBookManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajackus/digital_library_book_management_system/DigitalLibraryBookManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajackus.digital_library_book_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibraryBookManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
