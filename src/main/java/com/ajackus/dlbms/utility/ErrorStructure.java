6c9ef411ee43f1e5256a8afb07b2733d9151c3f5
David7557 <davidkumar7557@gmail.com>
2025-03-28 16:46:51 +0530

    creating errorStructure class

---
+++ src/main/java/com/ajackus/dlbms/utility/ErrorStructure.java
@@ -0,0 +1,14 @@
+package com.ajackus.dlbms.utility;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorStructure {
+    private String error;
+}
